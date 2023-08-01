d963ad0c87f41ace5bd1fb623b93c70b28b641be
andisety <purnomoandi126@gmail.com>
2023-08-01 10:12:19 +0700

    fix

--- core/build/generated/source/kapt/debug/hilt_aggregated_deps/_com_andi_amov_core_di_DatabaseModule.java
+++ core/build/generated/source/kapt/debug/hilt_aggregated_deps/_com_andi_amov_core_di_DatabaseModule.java
@@ -1,6 +1,7 @@
 package hilt_aggregated_deps;
 
 import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
+import javax.annotation.processing.Generated;
 
 /**
  * This class should only be referenced by generated code!This class aggregates information across multiple compilations.
@@ -9,5 +10,6 @@ import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
     components = "dagger.hilt.components.SingletonComponent",
     modules = "com.andi.amov.core.di.DatabaseModule"
 )
+@Generated("dagger.hilt.processor.internal.aggregateddeps.AggregatedDepsGenerator")
 public class _com_andi_amov_core_di_DatabaseModule {
 }
