c756cffe1c3a2307f2be0f7b7fa4a5da0a914af8
andisety <purnomoandi126@gmail.com>
2023-08-01 10:41:14 +0700

    fix

--- core/build/generated/source/kapt/debug/com/andi/amov/core/data/source/local/room/MovieDao_Impl.java
+++ core/build/generated/source/kapt/debug/com/andi/amov/core/data/source/local/room/MovieDao_Impl.java
@@ -20,12 +20,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
-import javax.annotation.processing.Generated;
 import kotlin.Unit;
 import kotlin.coroutines.Continuation;
 import kotlinx.coroutines.flow.Flow;
 
-@Generated("androidx.room.RoomProcessor")
 @SuppressWarnings({"unchecked", "deprecation"})
 public final class MovieDao_Impl implements MovieDao {
   private final RoomDatabase __db;
--- core/build/generated/source/kapt/debug/com/andi/amov/core/data/source/local/room/MovieDatabase_Impl.java
+++ core/build/generated/source/kapt/debug/com/andi/amov/core/data/source/local/room/MovieDatabase_Impl.java
@@ -23,9 +23,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import javax.annotation.processing.Generated;
 
-@Generated("androidx.room.RoomProcessor")
 @SuppressWarnings({"unchecked", "deprecation"})
 public final class MovieDatabase_Impl extends MovieDatabase {
   private volatile MovieDao _movieDao;
